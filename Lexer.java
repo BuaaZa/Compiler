73c63de1c7552a0fea1e65897ec1af400bd7cc74
Clover-0 <1051869687@qq.com>
2021-10-06 15:17:46 +0800

    test

---
+++ Lexer.java
@@ -0,0 +1,9 @@
+public class Lexer {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while(s.h)
+        int b = s.nextInt();
+        System.out.println(a+b);
+        s.close();
+    }
+}
--- Test.java
+++ Test.java
@@ -1,11 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
  * Test
  */
 public class Test {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File(args[0]);
+        Scanner s = new Scanner(file);
         int a = s.nextInt();
         int b = s.nextInt();
         System.out.println(a+b);
