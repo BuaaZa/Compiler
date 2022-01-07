e8dcfb76e2f30d4549d854da0cee6a93e62476fa
Clover-0 <1051869687@qq.com>
2022-01-07 20:26:52 +0800

    new

--- Compiler.java
+++ Compiler.java
@@ -138,12 +138,12 @@ public class Compiler {
                 arrayDimensions.add(exp.value);
             }
 
-            Variable param = new Variable(name,Symbol.TypePointer,++varList.blockNum,index,arrayDimensions,false,true);
+            Variable param = new Variable(name,Symbol.TypePointer,blockNum+1,index,arrayDimensions,false,true);
             function.addArg(param);
             res.append(param.getArrayAllocaInfo(arrayDimensions.size())).append("* ").append(param);
 
         }else{
-            Variable param =new Variable(name,Symbol.TypeInt,++varList.blockNum,index,true);
+            Variable param =new Variable(name,Symbol.TypeInt,blockNum+1,index,true);
             function.addArg(param);
             res.append("i32 ").append(param);
 
